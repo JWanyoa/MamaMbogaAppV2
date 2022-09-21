b58c28b057e5f7ccbcfca17fb28c55303aef04a5
JWanyoa <joel@kwelicapital.com>
2022-09-21 10:00:44 +0300

    With working registration template

--- resources/js/components/Register.vue
+++ resources/js/components/Register.vue
@@ -1,63 +1,87 @@
 <template>
-    <h1>Register</h1>
     <div class="container">
-        <form class="row row-cols-lg-auto g-3 align-items-center">
-            <div class="col-md-4">
-                <label class="visually-hidden" for="inlineFormInputGroupFirstname">Firstname</label>
-                <div class="input-group">
-                <div class="input-group-text">FN</div>
-                <input type="text" name="first_name" id="first_name" v-model="user.first_name" class="form-control" id="inlineFormInputGroupFirstname" placeholder="First Name">
-                </div>
-            </div>
+        <div class="card m-4">
+            <div class="card-body">
+                <h1 class="display-6 text-center m-2">Register</h1>
+                <form class="row g-3 align-items-center" @submit="register">
+                    <div class="col-md-4">
+                        <label class="visually-hidden" for="first_name">Firstname</label>
+                        <div class="input-group">
+                            <div class="input-group-text">FN</div>
+                            <input type="text" name="first_name" id="first_name" v-model="user.first_name" class="form-control" placeholder="First Name">
+                        </div>
+                    </div>
 
-            <div class="col-md-4">
-                <label class="visually-hidden" for="inlineFormInputGroupMiddlename">Middle Name</label>
-                <div class="input-group">
-                <div class="input-group-text">MN</div>
-                <input type="text" name="middle_name" id="middle_name" v-model="user.middle_name" class="form-control" id="inlineFormInputGroupMiddlename" placeholder="Middle Name">
-                </div>
-            </div>
+                    <div class="col-md-4">
+                        <label class="visually-hidden" for="middle_name">Middle Name</label>
+                        <div class="input-group">
+                            <div class="input-group-text">MN</div>
+                            <input type="text" name="middle_name" id="middle_name" v-model="user.middle_name" class="form-control" placeholder="Middle Name">
+                        </div>
+                    </div>
 
-            <div class="col-md-4">
-                <label class="visually-hidden" for="inlineFormInputGroupLastname">Last Name</label>
-                <div class="input-group">
-                <div class="input-group-text">LN</div>
-                <input type="text" name="last_name" id="last_name" v-model="user.last_name" class="form-control" id="inlineFormInputGroupLastname" placeholder="Last Name">
-                </div>
-            </div>
+                    <div class="col-md-4">
+                        <label class="visually-hidden" for="last_name">Last Name</label>
+                        <div class="input-group">
+                            <div class="input-group-text">LN</div>
+                            <input type="text" name="last_name" id="last_name" v-model="user.last_name" class="form-control" placeholder="Last Name">
+                        </div>
+                    </div>
 
-            
-            <div class="col-12">
-                <label class="visually-hidden" for="inlineFormInputGroupUsername">Username</label>
-                <div class="input-group">
-                <div class="input-group-text">@</div>
-                <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Username">
-                </div>
-            </div>
+                    
+                    <div class="col-md-12">
+                        <label class="visually-hidden" for="user_name">Username</label>
+                        <div class="input-group">
+                            <div class="input-group-text">@</div>
+                            <input type="text" class="form-control" name="username" id="username" v-model="user.username" placeholder="Username">
+                        </div>
+                    </div>
 
-            <div class="col-12">
-                <label class="visually-hidden" for="inlineFormSelectPref">Role</label>
-                <select class="form-select" id="inlineFormSelectPref">
-                <option selected>Choose...</option>
-                <option value="1">Admin</option>
-                <option value="2">Customer</option>
-                <option value="3">Supplier</option>
-                </select>
-            </div>
+                    <div class="col-md-12">
+                        <label class="visually-hidden" for="user_name">Email</label>
+                        <div class="input-group">
+                            <div class="input-group-text"><i class="fa fa-envelope"></i></div>
+                            <input type="text" class="form-control" name="email" id="email" v-model="user.email" placeholder="email">
+                        </div>
+                    </div>
 
-            <div class="col-12">
-                <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="inlineFormCheck">
-                <label class="form-check-label" for="inlineFormCheck">
-                    Remember me
-                </label>
-                </div>
-            </div>
+                    <div class="col-md-12">
+                        <label class="visually-hidden" for="user_name">Password</label>
+                        <div class="input-group">
+                            <div class="input-group-text"><i class="fa fa-key"></i></div>
+                            <input type="text" class="form-control" name="password" id="password" v-model="user.password" placeholder="password">
+                        </div>
+                    </div>
 
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                    <div class="col-md-12">
+                        <label class="visually-hidden" for="role">Role</label>
+                        <div class="input-text">
+                            Role
+                           <select class="form-select" id="role" name="role" v-model="user.role">
+                                <option selected>Choose...</option>
+                                <option value="1">Admin</option>
+                                <option value="2">Customer</option>
+                                <option value="3">Supplier</option>
+                            </select> 
+                        </div>
+                        
+                    </div>
+
+                    <div class="col-md-12">
+                        <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="token" name="token" v-model="user.token">
+                        <label class="form-check-label" for="token">
+                            Remember me
+                        </label>
+                        </div>
+                    </div>
+
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
             </div>
-        </form>
+        </div>
     </div>
 </template>
 
@@ -82,7 +106,7 @@ export default({
                 email: '',
                 password: '',
                 role: '',
-                token: NULL
+                token: ''
             }
         }
     },
